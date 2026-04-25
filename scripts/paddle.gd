d21960894a7ccad68d648568c6240d715e497549
sarahmmcb <sarah.m.mcb@gmail.com>
2026-04-24 22:09:21 -0400

    Add some notes on paddle physics

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -28,3 +28,23 @@ func _physics_process(delta: float) -> void:
 	
 
 	move_and_slide()
+	
+	
+# Comments on YouTube note the problem with the paddle being pushed down
+# Someone said it's because you shouldn't use CharacterBody2D for something 
+# so simple as a paddle that just moves left and right. Rather, use
+# RigidBody2D or even StaticBody 2D with manual logic:
+	
+#extends StaticBody2D
+#
+#@export var speed := 500  # Move speed
+#
+#func _process(delta):
+	#var move_direction = 0
+	#if Input.is_action_pressed("ui_left"):
+		#move_direction = -1
+	#elif Input.is_action_pressed("ui_right"):
+		#move_direction = 1
+#
+	#position.x += move_direction * speed * delta
+	#position.x = clamp(position.x, 50, 550)  # Keep it inside screen (I set my screen width 600 pixels) 
